31784dd79b2e5c52f448114f90524900ce708452
bryja <bryja@DESKTOP-H4T5KIH>
2026-09-07 11:30:17 +0100

    Add project files.

---
+++ InventoryApi/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryApi
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
